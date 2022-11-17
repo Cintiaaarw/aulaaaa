f5a1568af4012711152df65e5b54f6675b047567
Cintiaaarw <cintiabrandaorw@gmail.com>
2022-11-16 20:12:24 -0400

    Meu primeiro Commit

---
+++ src/tipos_de_variaveis/TiposVariaveis.java
@@ -0,0 +1,20 @@
+package tipos_de_variaveis;
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		// Tipos de variaveis
+		
+		int idade = 26;
+		double salario = 1250.50;
+		float salarioFuncionario = 12350.0f;
+		boolean maiorDeIdade = false;
+		boolean menorDeIdade = true;
+		String fraseOuTexto = "Hello word";
+		char caracteres = 'g';
+		System.out.println("cintia tem "+ idade +"anos");
+		System.out.println("ganha " + salario);
+		
+	
+		
+	}
+}
